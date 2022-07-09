aacc71b280e8a6896656ddb584eaec86b0154c1e
tingwei0518 <tingwei0518@gmail.com>
2022-07-09 21:53:02 +0800

    feat: menu layout

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,33 +2,76 @@ import { useContext } from 'react';
 import { getAuth, signOut } from 'firebase/auth';
 import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
+// import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
 import UserContext from '../../UserContext';
+import barCode from '../../assets/barcode.svg';
+import title from '../../assets/title.svg';
+import menuBackground from '../../assets/menu.svg';
+// import slash from '../../images/slash.svg';
 
 const Wrapper = styled.div`
   width: 300px;
   height: 100vh;
   position: fixed;
-  background-color: white;
+  background-image: url(${menuBackground});
+  background-size: cover;
+  background-position: right 5%;
+  background-repeat: no-repeat;
   top: 0;
   left: 0;
   z-index: 4;
-  box-shadow: 8px 0 5px rgba(0, 0, 0, 0.2);
+  filter: drop-shadow(8px 0 5px rgba(0, 0, 0, 0.2));
   display: ${(props) => (props.isOpen ? 'flex' : 'none')};
-  padding: 50px 30px;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px 0;
+`;
+
+// const TodayDate = styled.div`
+//   font-size: 3rem;
+//   font-family: Times,sans-serif;
+//   text-align: right;
+//   color: rgba(255, 255, 255, .8);
+//   background-image: url(${slash});
+//   background-size: cover;
+//   background-position: top 5px;
+//   background-repeat: no-repeat;
+//   position: absolute;
+//   bottom: 4%;
+//   right: 13%;
+//   span {
+//     display: block;
+//     font-size: 2rem;
+//   }
+// `;
+
+const Copyright = styled.div`
+  font-size: .8rem;
 `;
 
 const MenuLists = styled.ul`
+  height: fit-content;
   list-style: none;
-  margin: 0;
-  padding: 0;
+  margin: 20px 0 0 -30px;
+  padding: 13px 0 0 20px;
+  border-left: .9px solid black;
+  font-size: 1.1rem;
   & a {
     color: black;
     text-decoration: none;
   }
   li {
-    margin-bottom: 10px;
+    margin-bottom: 13px;
     cursor: pointer;
+    :hover {
+      font-weight: bold;
+    }
+  }
+  button {
+    :hover{
+      font-weight: bold;
+    }
   }
 `;
 
@@ -39,9 +82,22 @@ function Menu({
 }) {
   const currentUser = useContext(UserContext);
   const auth = getAuth();
+  // const monthsName = [
+  // eslint-disable-next-line max-len
+  //   'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'
+  // ];
+  // const month = monthsName[Number(dayjs().format('M')) - 1];
+  // const day = dayjs().format('DD');
 
   return (
     <Wrapper isOpen={isOpen}>
+      <Copyright>Copyright © 2022 Arts Everywhere</Copyright>
+      <img src={barCode} alt="barcode" style={{ width: '200px' }} />
+      <img src={title} alt="arts everywhere" style={{ width: '200px', marginTop: '80px' }} />
+      {/* <TodayDate>
+        {month}
+        <span>{day}</span>
+      </TodayDate> */}
       <MenuLists>
         {
           (currentUser.userId === '')
@@ -51,12 +107,12 @@ function Menu({
               </li>
             ) : (
               <li onClick={() => setIsOpen(!isOpen)} aria-hidden="true">
-                <Link to="/profile">會員專區</Link>
+                <Link to="/profile">{`會員專區(${currentUser.userName})`}</Link>
               </li>
             )
         }
         <li onClick={() => scrollToElement(filteredInfoRef)} aria-hidden="true">
-          結果地圖
+          搜尋結果地圖
         </li>
         {
           isFiltered
@@ -73,10 +129,10 @@ function Menu({
           熱門活動
         </li>
         <li onClick={() => scrollToElement(userEventsRef)} aria-hidden="true">
-          會員活動
+          會員刊登活動
         </li>
         <li onClick={() => scrollToElement(userEventsEditorRef)} aria-hidden="true">
-          刊登編輯區
+          活動刊登編輯區
         </li>
         <li onClick={() => scrollToElement(homeRef)} aria-hidden="true">
           回首頁
@@ -87,6 +143,9 @@ function Menu({
             <li onClick={() => setIsOpen(!isOpen)} aria-hidden="true">
               <button
                 type="submit"
+                style={{
+                  fontSize: '1rem', padding: '0', border: 'none', background: 'none', cursor: 'pointer',
+                }}
                 onClick={() => signOut(auth).then(() => {
                   console.log('Sign-out successful.');
                 }).catch((error) => {
